1e5c344bf5f2d57667b7ea6b70ea649fb70ff81e
damiankliman <klimandamian@gmail.com>
2025-02-08 12:18:51 -0800

    add main container

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useThingSpeakData } from "@/hooks/useThingSpeakData";
-import { HeaderContainer } from "./styles";
+import { HeaderContainer, MainContainer } from "./styles";
 
 function App() {
   // const { data } = useThingSpeakData(2605389, "2025-02-08T07:40:25Z");
@@ -9,6 +9,7 @@ function App() {
       <HeaderContainer>
         <h1>B IS FOR BOAT MONITOR</h1>
       </HeaderContainer>
+      <MainContainer></MainContainer>
     </>
   );
 }
--- src/styles.ts
+++ src/styles.ts
@@ -4,6 +4,17 @@ export const HeaderContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 16px 0;
+  padding: 16px;
   border-bottom: 1px solid var(--color-border);
+
+  > h1 {
+    text-align: center;
+  }
+`;
+
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 16px;
 `;
