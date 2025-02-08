a5ddf0bb18b66f62939d10cd8f0e2edec7d6bd3d
damiankliman <klimandamian@gmail.com>
2025-02-08 14:05:31 -0800

    component for each boat

---
+++ src/components/BoatMonitor/styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const BoatMonitorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  width: 100%;
+`;
+
+export const ChartsHeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+export const ChartsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  width: 100%;
+`;
--- src/styles.ts
+++ src/styles.ts
@@ -22,16 +22,3 @@ export const MainContainer = styled.div`
   max-width: 800px;
   margin: 0 auto;
 `;
-
-export const ChartsHeaderContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-`;
-
-export const ChartsContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-  width: 100%;
-`;
